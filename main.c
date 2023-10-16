f8f83d6ea1ae80f86adf847e66d5186c6219ef86
Matikilkil <lydiahjerotichkipchumba@gmail.com>
2023-10-16 09:14:58 -0700

    files added

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
 /**
- * main - entry point
+ * main - the entry point
  * @ac: arg count
  * @av: arg vector
- *
  * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
