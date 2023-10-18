6b9fff10b95648dff47f1dba91c83d42ff61ddad
Nyamburavee <veronyambura716@gmail.com>
2023-10-17 19:49:35 -0700

    Updates

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "shell.h"
 
 /**
- * main - the entry point
+ * main -The  entry point
  * @ac: arg count
  * @av: arg vector
+ *
  * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
@@ -38,6 +39,6 @@ int main(int ac, char **av)
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
+
